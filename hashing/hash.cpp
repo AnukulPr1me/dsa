80bb509edbec68ad67a3520ed5d7b0ecbdf6f8d3
AnukulPr1me <anukul_chandra@outlook.com>
2024-06-24 12:10:41 +0530

    added array hashing

--- hashing/hash.cpp
+++ hashing/hash.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-
+#include <vector>
+#include <map>
+#include <string>
 using namespace std;
 
 void counterNum(int n, int arr[]){
@@ -12,11 +14,57 @@ void counterNum(int n, int arr[]){
     cout << counter;
 }
 
+void stringHash(){
+    string s;
+    cin>>s;
+
+    int hash[256] = {0};
+    for (int i = 0; i < s.size(); i++)
+    {
+        hash[s[i]++];
+    }
+
+    int q;
+    cin>>q;
+    while(q--) {
+        char c;
+        cin >> c;
+        cout << hash[c] << endl;
+    }
+    
+
+}
+void mapHash() {
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    map<int,int>mpp;
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr[i]]++;
+    }
+    int q;
+    cin >>q;
+    while(q--) {
+        int number;
+        cin>>number;
+        cout << mpp[number] << endl;
+    }
+    
+    
+}
+
 
 int main() {
-    int arr[] = {4, 5, 7, 4, 10};
-    int n = 4;
-    counterNum(n, arr);
+    // int arr[] = {4, 5, 7, 4, 10};
+    // int n = 4;
+    // counterNum(n, arr);
+    mapHash();
     return 0;
 
 }
