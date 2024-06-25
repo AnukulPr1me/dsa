a57b97a3a62e044468b7dc7b246b8215d5ed20c0
AnukulPr1me <anukul_chandra@outlook.com>
2024-06-25 19:26:49 +0530

    insertion sort

--- sorting/sorting.cpp
+++ sorting/sorting.cpp
@@ -31,6 +31,22 @@ void bubbleSort(vector<int>& arr){
     }
 }
 
+void insertionSort(vector<int>& arr){
+    int n = arr.size();
+    for(int i=0; i<n-1; i++){
+        int key = arr[i];
+        int j = i-1;
+
+        while(j >= 0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+        
+    }
+
+}
+
 int main() {
 
     vector<int> arr = {64, 25, 12, 22, 11};
@@ -46,6 +62,13 @@ int main() {
     bubbleSort(arr);
     for(int i: arr){
         cout << i << " ";
+    }
+    
+    cout << endl;
+
+    insertionSort(arr);
+    for(int j: arr){
+        cout << j << " ";
     }
 
 
