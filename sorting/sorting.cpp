1338f06a34b4739c6fee65a946fd8e8db29fe529
AnukulPr1me <anukul_chandra@outlook.com>
2024-06-25 22:26:56 +0530

    merge sort

--- sorting/sorting.cpp
+++ sorting/sorting.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -47,10 +47,58 @@ void insertionSort(vector<int>& arr){
 
 }
 
+
+// Merge sort 
+
+void merge(vector<int>& arr, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        } else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int>& arr, int low, int high) {
+    if (low >= high) {
+        return;
+    }
+
+    int mid = low + (high - low) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+void MergeSort(vector<int>& arr, int n) {
+    mergeSort(arr, 0, n - 1);
+}
+
 int main() {
 
     vector<int> arr = {64, 25, 12, 22, 11};
-
+    int n = arr.size();
     selectionSort(arr);
 
     for(int i : arr){
@@ -63,13 +111,22 @@ int main() {
     for(int i: arr){
         cout << i << " ";
     }
-    
+
     cout << endl;
 
     insertionSort(arr);
     for(int j: arr){
         cout << j << " ";
     }
+
+    cout << endl;
+
+    MergeSort(arr, n);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    cout << endl;
 
 
 }
