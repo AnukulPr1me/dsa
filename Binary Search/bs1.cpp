06a0a6ef3a8f09f2949362a55020d359637d3fed
anukulpr1me <anukul_chandra@outlook.com>
2024-07-15 22:26:41 +0530

    Iterative

---
+++ Binary Search/bs1.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int search(vector<int> & nums){
+    int n = nums.size();
+    int target = 8;
+    int low=0;
+    int high = n-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(nums[mid] == target) {
+            return mid;
+        }
+        else if(target>nums[mid]){
+            low = mid+1;
+        }
+        else{
+            high = mid -1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> nums = { 1, 4, 5, 8, 9, 10};
+    
+    int result = search(nums);
+
+    cout << result;
+    return 0;
+}
